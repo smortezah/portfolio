d3ce576b85542d22a8e1f95c917517c8f2c5ddb9
smortezah <s_morteza_h@yahoo.com>
2023-10-02 11:06:49 +0100

    Warn on broken links

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: 'smortezah', // Usually your GitHub org/user name.
   projectName: 'portfolio', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
