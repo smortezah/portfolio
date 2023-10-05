8f81dc523bb20ef0b39a7b0a2f790786d497999f
smortezah <s_morteza_h@yahoo.com>
2023-10-05 23:27:19 +0100

    Modify footer and navbar

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,8 +8,9 @@ const katex = require("rehype-katex");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "Portfolio",
+  tagline:
+    "Various projects on applications of Data Science and Machine Learning",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
@@ -69,7 +70,7 @@ const config = {
   markdown: {
     mermaid: true,
   },
-  
+
   themes: ["@docusaurus/theme-mermaid"],
 
   themeConfig:
@@ -80,17 +81,18 @@ const config = {
       navbar: {
         title: "Portfolio",
         items: [
-          {
-            label: "Tutorial",
-            type: "docSidebar",
-            sidebarId: "tutorialSidebar",
-            position: "left",
-          },
+          // {
+          //   label: "Tutorial",
+          //   type: "docSidebar",
+          //   sidebarId: "tutorialSidebar",
+          //   position: "left",
+          // },
           // { to: "/blog", label: "Blog", position: "left" },
           {
-            label: "GitHub",
-            href: "https://github.com/smortezah/portfolio",
             position: "right",
+            href: "https://github.com/smortezah/portfolio",
+            className: "header-github-link",
+            "aria-label": "GitHub repository",
           },
         ],
       },
@@ -98,29 +100,69 @@ const config = {
         style: "dark",
         links: [
           {
-            label: "Medium",
-            href: "https://medium.com/@morihosseini",
+            html: `
+              <a
+                class="footer-medium-link"
+                href="https://medium.com/@morihosseini"
+                target="_blank"
+                rel="noreferrer noopener"
+                aria-label="Medium"
+              >
+              </a>
+            `,
           },
           {
-            label: "Medium",
-            href: "https://medium.com/@morihosseini",
+            html: `
+              <a
+                class="footer-github-link"
+                href="https://github.com/smortezah"
+                target="_blank"
+                rel="noreferrer noopener"
+                aria-label="GitHub"
+              >
+              </a>
+            `,
           },
           // {
           //   html: `
-          //     <div width=160px height=28px gap="1rem" display="flex">
-          //       <a href="https://github.com/smortezah" target="_blank" rel="noopener noreferrer"><img src="img/github.svg"></a>
-          //       <a href="https://linkedin.com/in/mori-hosseini" target="_blank" rel="noopener noreferrer"><img src="img/linkedin.svg"></a>
-          //       <a href="https://x.com/MoriHosseini1" target="_blank" rel="noopener noreferrer"><img src="img/x.svg"></a>
-          //     </div>
+          //     <a
+          //       href="https://github.com/smortezah"
+          //       target="_blank"
+          //       rel="noreferrer noopener"
+          //       aria-label="GitHub"
+          //     >
+          //       <svg width="24" height="24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          //         <path d='M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12' />
+          //       </svg> 
+          //     </a>
           //   `,
           // },
-          // {
-          //   html: `
-          //   <a href="https://www.linkedin.com/in/mori-hosseini" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn"><svg viewBox="0 0 24 24" aria-hidden="true" focusable="false" fill="currentColor" xmlns="http://www.w3.org/2000/svg" class="StyledIconBase-sc-ea9ulj-0 ebjPRL h-7 w-7 text-zinc-400 hover:text-primary"><circle cx="4.983" cy="5.009" r="2.188"></circle><path d="M9.237 8.855v12.139h3.769v-6.003c0-1.584.298-3.118 2.262-3.118 1.937 0 1.961 1.811 1.961 3.218v5.904H21v-6.657c0-3.27-.704-5.783-4.526-5.783-1.835 0-3.065 1.007-3.568 1.96h-.051v-1.66H9.237zm-6.142 0H6.87v12.139H3.095z"></path></svg></a>
-          //   `,
-          // }
+          {
+            html: `
+              <a
+                class="footer-x-link"
+                href="https://x.com/MoriHosseini1"
+                target="_blank"
+                rel="noreferrer noopener"
+                aria-label="X"
+              >
+              </a>
+            `,
+          },
+          {
+            html: `
+              <a
+                class="footer-linkedin-link"
+                href="https://linkedin.com/in/mori-hosseini"
+                target="_blank"
+                rel="noreferrer noopener"
+                aria-label="LinkedIn"
+              >
+              </a>
+            `,
+          },
         ],
-        // copyright: `Copyright © ${new Date().getFullYear()} Morteza Hosseini`,
+        copyright: `Copyright &copy; ${new Date().getFullYear()} Morteza Hosseini`,
       },
       prism: {
         theme: lightCodeTheme,
