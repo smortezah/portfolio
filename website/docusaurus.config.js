5b528f046c872c844e41c67461ad177acbd35c48
smortezah <s_morteza_h@yahoo.com>
2023-11-06 14:58:17 +0000

    Add Algolia search functionality to website

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -168,6 +168,35 @@ const config = {
       mermaid: {
         theme: { light: "neutral", dark: "forest" },
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: ${{ secrets.ALGOLIA_APPID }},
+  
+        // Public API key: it is safe to commit it
+        apiKey: 'bfaea2ddcc75781fc2ba26ec3aedc985',
+  
+        indexName: 'portfolio',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
+      },  
     }),
 };
 
