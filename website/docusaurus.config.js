ae1c715c60ef0e0d686c94b2eee4ece0deaacad4
Morteza Hosseini <s_morteza_h@yahoo.com>
2025-10-28 22:39:59 +0000

    fix: update blog routeBasePath in docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -42,14 +42,13 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       {
         docs: {
-          routeBasePath: '/docs', // Serve the docs at the site's root
-          // routeBasePath: '/', // Serve the docs at the site's root
-          // path: "docs",
+          routeBasePath: '/docs',
           remarkPlugins: [remarkMath],
           rehypePlugins: [rehypeKatex],
           sidebarPath: require.resolve("./sidebars.js"),
         },
         blog: {
+          routeBasePath: '/blog',
           showReadingTime: true,
         },
         theme: {
